e6a77cd902bcf1822db0fc7db56598dfe7c2e851
Zhantore <zhantore29@gmail.com>
2026-02-22 21:53:05 +0500

    update

--- assignment3/golang/database/migrations/000002_init.up.sql
+++ assignment3/golang/database/migrations/000002_init.up.sql
@@ -1,6 +1,9 @@
-create table if not exists users (
-  id serial primary key,
-  name varchar(255) not null
-);
-
-insert into users (name) values ('John Doe');
+create table if not exists users ( 
+  id serial primary key, 
+  name varchar(255) not null, 
+  email varchar(255) not null, 
+  city varchar(255) not null, 
+  age int not null 
+); 
+ 
+insert into users (name, email, city, age) values ('John Doe', 'john@example.com', 'New York', 30);
